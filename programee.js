b3b63fc07a17a4b68193ccff0de6165ab9b98296
Rojeets <152057235+Rojeets@users.noreply.github.com>
2023-12-02 18:31:58 +0545

    Update programee.js

--- programee.js
+++ programee.js
@@ -1,12 +1,12 @@
 function OddEven() {
     var newPage = window.open('', 'Wap to find odd or even.');
-    var cCode = "#include '<stdio.h>'\n\nint main() {\n\tint number;\n\n\t// Input from the user\n\tprintf(\"Enter an integer: \");\n\tscanf(\"%d\", &number);\n\n\t// Check if the number is even or odd\n\tif (number % 2 == 0) {\n\t\tprintf(\"%d is even.\\n\", number);\n\t} else {\n\t\tprintf(\"%d is odd.\\n\", number);\n\t}\n\n\treturn 0;\n}";
+    var cCode = "#include '<stdio.h>'\nvoid main() {\n\tint number;\n\tprintf(\"Enter an integer: \");\n\tscanf(\"%d\", &number);\n\tif (number % 2 == 0) {\n\t\tprintf(\"%d is even.\\n\", number);\n\t} else {\n\t\tprintf(\"%d is odd.\\n\", number);\n\t}\n\n}";
     newPage.document.write("<h1>C Code:</h1>");
     newPage.document.write("<pre>" + cCode + "</pre>");
 }
 function Positive(){
     var newPage = window.open('', 'find positive negative or zero');
-    var cCode ="#include <stdio.h>\nvoid main() {\n\tint number;\n\tprintf(\"Enter a number: \");\n\tscanf(\"%d\", &number);\n\tif (number > 0)\n\t\t\tprintf(\"The entered number is positive.\n\"); \n\telse if (number < 0) \n\t\t\tprintf(\"The entered number is negative.\n\");\n\telse \n\t\tprintf(\"The entered number is zero.\n\");\n\t return 0;\n}";
+    var cCode ="#include <stdio.h>\nvoid main() {\n\tint number;\n\tprintf(\"Enter a number: \");\n\tscanf(\"%d\", &number);\n\tif (number > 0){\n\t\tprintf(\"The entered number is positive.\\n\");\n\t} \n\telse if (number < 0) {\n\t\tprintf(\"The entered number is negative.\\n\");\n\t}\n\telse {\n\t\tprintf(\"The entered number is zero.\\n\");\n\t}\n}";
     newPage.document.write("<h1>C Code</h1>");
     newPage.document.write("<pre>" + cCode + "</pre>");
 
